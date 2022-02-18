595da698ee234b65f91143a797da5eb4261a2bec
MikakoKoike <mikako.koike@rakus-partners.co.jp>
2022-02-18 15:56:15 +0900

    競合動作確認のためEmployee.tsを修正

--- src/types/Employee.ts
+++ src/types/Employee.ts
@@ -1,11 +1,11 @@
 export class Employee {
-  constructor(private _name: string) {}
+  constructor(private _name2: string) {}
 
-  public get name(): string {
-    return this._name;
+  public get name2(): string {
+    return this._name2;
   }
 
-  public set name(name: string) {
-    this._name = name;
+  public set name2(name2: string) {
+    this._name2 = name2;
   }
 }
